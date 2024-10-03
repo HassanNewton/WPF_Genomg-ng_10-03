253c66dba64d5b86107964e7009deda4ea3cad5f
HassanNewton <hassan.hussin@newton.se>
2024-10-03 09:47:35 +0200

    Add project files.

---
+++ WPF_Genomgång_10-03/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace WPF_Genomgång_10_03
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NavigateToStart(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new StartPage());
+        }
+
+        private void NavigateToInformation(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new InformationPage());
+        }
+
+        private void NavigateToOmOss(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new OmossPage());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {   //Variabel som sparar värdet av det som matas in
+            string Name = InputBox.Text;
+            MainFrame.Navigate(new ShowPage(Name));
+            //Skicka namnet till nästa page
+        }
+    }
+}
---
+++ WPF_Genomgång_10-03/ShowPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace WPF_Genomgång_10_03
+{
+    /// <summary>
+    /// Interaction logic for ShowPage.xaml
+    /// </summary>
+    public partial class ShowPage : Page
+    {   //Lagt till en parameter name
+        public ShowPage(string Name)
+        {
+            InitializeComponent();
+            txtBlockShow.Text = $"Välkommen, {Name}";
+        }
+    }
+}
