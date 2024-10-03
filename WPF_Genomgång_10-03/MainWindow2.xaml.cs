1dc47c4d330d67e07a1ecb0e7b43aa39ed067ab8
HassanNewton <hassan.hussin@newton.se>
2024-10-03 10:25:13 +0200

    Flera Windows exempel

---
+++ WPF_Genomgång_10-03/MainWindow2.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace WPF_Genomgång_10_03
+{
+    /// <summary>
+    /// Interaction logic for MainWindow2.xaml
+    /// </summary>
+    public partial class MainWindow2 : Window
+    {
+        public MainWindow2()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenWelcomeWindow(object sender, RoutedEventArgs e)
+        {
+            //Skapar en ny instans av fönstret
+            WelcomeWindow welcomeWindow = new WelcomeWindow();
+            welcomeWindow.Show();
+        }
+
+        private void OpenInfoWindow(object sender, RoutedEventArgs e)
+        {
+            //Skapar en ny instans av fönstret
+            InfoWindow infoWindow = new InfoWindow();
+            infoWindow.Show();
+        }
+    }
+}
